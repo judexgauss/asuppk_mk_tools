6a267a035e8647895821cc0065e806a3ca6ad240
Гуськов Евгений Вячеславович <guskov@office.promit-ek.ru>
2024-06-03 12:41:29 +0500

    added ability to define IPv4 user address

--- dbmake.ps1
+++ dbmake.ps1
@@ -7,6 +7,21 @@ adb shell "rm /sdcard/MobileBooking/PFR/*.db";
 adb shell "rm /sdcard/MobileBooking/mkassir.db";
 echo "done";
 
-echo "trying to add server-addr.txt"
+if ([string]::IsNullOrWhitespace($asuppk_srv_ip)) {
+	echo "Didn't get any ip from you. Looking for IPv4 Address"
+	$asuppk_srv_ip = (
+    Get-NetIPConfiguration |
+    Where-Object {
+        $_.IPv4DefaultGateway -ne $null -and
+        $_.NetAdapter.Status -ne "Disconnected"
+    }
+).IPv4Address.IPAddress
+	$asuppk_srv_ip += ";"
+	echo "Found your IPv4 Address: '$asuppk_srv_ip'"
+}
+
+
+
+echo "trying to add '$asuppk_srv_ip' to server-addr.txt"
 adb shell "echo '$asuppk_srv_ip' > /sdcard/MobileBooking/server-addr.txt";
 echo "done";
